49b4859636d0e1d3cb8d87edb2aeabe81e287f84
ADjakonow <63237348+ADjakonow@users.noreply.github.com>
2020-04-06 13:39:34 +0200

    Add files via upload

---
+++ wyklad_5/build/CMakeFiles/PET.dir/DependInfo.cmake
@@ -0,0 +1,58 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home2/aneta/UW/geant/prace_domowe/wyklad_5/PET.cc" "/home2/aneta/UW/geant/prace_domowe/wyklad_5/build/CMakeFiles/PET.dir/PET.cc.o"
+  "/home2/aneta/UW/geant/prace_domowe/wyklad_5/src/DetectorConstruction.cc" "/home2/aneta/UW/geant/prace_domowe/wyklad_5/build/CMakeFiles/PET.dir/src/DetectorConstruction.cc.o"
+  "/home2/aneta/UW/geant/prace_domowe/wyklad_5/src/EMPhysics.cc" "/home2/aneta/UW/geant/prace_domowe/wyklad_5/build/CMakeFiles/PET.dir/src/EMPhysics.cc.o"
+  "/home2/aneta/UW/geant/prace_domowe/wyklad_5/src/EventAction.cc" "/home2/aneta/UW/geant/prace_domowe/wyklad_5/build/CMakeFiles/PET.dir/src/EventAction.cc.o"
+  "/home2/aneta/UW/geant/prace_domowe/wyklad_5/src/GeneralPhysics.cc" "/home2/aneta/UW/geant/prace_domowe/wyklad_5/build/CMakeFiles/PET.dir/src/GeneralPhysics.cc.o"
+  "/home2/aneta/UW/geant/prace_domowe/wyklad_5/src/HumanFantom.cc" "/home2/aneta/UW/geant/prace_domowe/wyklad_5/build/CMakeFiles/PET.dir/src/HumanFantom.cc.o"
+  "/home2/aneta/UW/geant/prace_domowe/wyklad_5/src/PhysicsList.cc" "/home2/aneta/UW/geant/prace_domowe/wyklad_5/build/CMakeFiles/PET.dir/src/PhysicsList.cc.o"
+  "/home2/aneta/UW/geant/prace_domowe/wyklad_5/src/PrimaryGeneratorAction.cc" "/home2/aneta/UW/geant/prace_domowe/wyklad_5/build/CMakeFiles/PET.dir/src/PrimaryGeneratorAction.cc.o"
+  "/home2/aneta/UW/geant/prace_domowe/wyklad_5/src/Ring.cc" "/home2/aneta/UW/geant/prace_domowe/wyklad_5/build/CMakeFiles/PET.dir/src/Ring.cc.o"
+  "/home2/aneta/UW/geant/prace_domowe/wyklad_5/src/RunAction.cc" "/home2/aneta/UW/geant/prace_domowe/wyklad_5/build/CMakeFiles/PET.dir/src/RunAction.cc.o"
+  "/home2/aneta/UW/geant/prace_domowe/wyklad_5/src/SteppingAction.cc" "/home2/aneta/UW/geant/prace_domowe/wyklad_5/build/CMakeFiles/PET.dir/src/SteppingAction.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "G4INTY_USE_QT"
+  "G4INTY_USE_XT"
+  "G4LIB_BUILD_DLL"
+  "G4UI_USE_QT"
+  "G4UI_USE_TCSH"
+  "G4VIS_USE_OPENGL"
+  "G4VIS_USE_OPENGLQT"
+  "G4VIS_USE_OPENGLX"
+  "QT_CORE_LIB"
+  "QT_GUI_LIB"
+  "QT_NO_DEBUG"
+  "QT_OPENGL_LIB"
+  "QT_PRINTSUPPORT_LIB"
+  "QT_WIDGETS_LIB"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home2/aneta/geant4/build/install/include/Geant4"
+  "../include"
+  "/home2/aneta/root/include"
+  "/usr/include/x86_64-linux-gnu/qt5"
+  "/usr/include/x86_64-linux-gnu/qt5/QtGui"
+  "/usr/include/x86_64-linux-gnu/qt5/QtCore"
+  "/usr/lib/x86_64-linux-gnu/qt5/mkspecs/linux-g++"
+  "/usr/include/x86_64-linux-gnu/qt5/QtWidgets"
+  "/usr/include/x86_64-linux-gnu/qt5/QtPrintSupport"
+  "/usr/include/x86_64-linux-gnu/qt5/QtOpenGL"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ wyklad_5/include/PrimaryGeneratorAction.hh
@@ -0,0 +1,34 @@
+//
+// $Id: PrimaryGeneratorAction.hh 15.10.2018 A Fijalkowska $
+//	+ A.Djakonow
+/// \file PrimaryGeneratorAction.hh
+/// \brief Definition of the PrimaryGeneratorAction class
+
+#ifndef PrimaryGeneratorAction_h
+#define PrimaryGeneratorAction_h 1
+
+#include "G4VUserPrimaryGeneratorAction.hh"
+#include "G4ParticleGun.hh"
+#include "G4ParticleTable.hh"
+
+class PrimaryGeneratorAction : public G4VUserPrimaryGeneratorAction
+{
+  public:
+    PrimaryGeneratorAction();
+    virtual ~PrimaryGeneratorAction();
+
+  public:
+    virtual void GeneratePrimaries(G4Event*);
+
+  private:
+	void SetUpDefault();	
+	G4ThreeVector GenerateIsotropicDirection();
+	void GeneratePositionIncident(G4Event*);
+    G4ParticleGun* particleGun;
+    G4ParticleTable* particleTable;
+
+};
+
+
+#endif 
+
