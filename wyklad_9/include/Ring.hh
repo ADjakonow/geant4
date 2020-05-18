e7a1a2268c9ace82b07dfb5d22a445c8a726d784
ADjakonow <63237348+ADjakonow@users.noreply.github.com>
2020-05-18 20:31:20 +0200

    Add files via upload

---
+++ wyklad_9/include/Ring.hh
@@ -0,0 +1,32 @@
+//Zadanie do wykladu 5
+//class Ring -- budowa rigu z polipropylenu z detektorami z NaI
+// A.Djakonow 3.04.2020
+
+
+#ifndef Ring_H
+#define Ring_H 1
+
+#include "G4Material.hh"
+#include "G4LogicalVolume.hh"
+#include "G4NistManager.hh"  //wbudowane materiały
+
+class Ring
+{
+	public:
+	Ring(double rMin, double rMax, double length, G4LogicalVolume* worldLogic);
+	void ConstructSDandField();
+	
+	private:
+	void CylinderBuild(double rMin, double rMax, double length, G4LogicalVolume* worldLogic);
+	void ConstructNaIDet();
+	G4LogicalVolume* ConstructAluLayer();
+	G4LogicalVolume* ConstructTeflonLayer();
+	G4LogicalVolume* ConstructNaI();
+
+	//G4LogicalVolume* worldLogic; //świat
+	G4NistManager* man=G4NistManager::Instance();
+ 	G4LogicalVolume* cylinderLogVol;
+    	G4LogicalVolume* naiLogic;
+};    
+
+#endif
